cf29c36ed69984570128b3f18f65cd73cd16b858
jmb-python-developer <165929314+jmb-python-developer@users.noreply.github.com>
2024-05-22 18:05:04 +0200

    Finished gradient descent notebook for Logistic Regression in Classification

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,34 +1,6 @@
 import copy, math
 import numpy as np
 
-def compute_gradient(X, y, w, b):
-    """
-    Args:
-      X (ndarray (m,n)): Data, m examples with n features. A Matrix.
-      y (ndarray (m,)) : target values
-      w (ndarray (n,)) : model parameters  
-      b (scalar)       : model parameter
-      
-    Returns:
-      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
-      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
-    """
-    m,n = X.shape # m number of examples, n number of features (column)
-    dj_db = 0.
-    dj_dw = np.zeros((n,))
-    for i in range(m):
-      # err is the derivative of the biased or b parameter, per row (i)
-      err = (np.dot(X[i], w) + b) - y[i]
-      # Inner loop does the derivative per each 'w' parameter with respect to it's affecting 'x' feature (j)
-      for j in range(n):
-        dj_dw[j] = dj_dw[j] + err * X[i, j] 
-      # Accumulates in outer loop updated b value, the partial derivative to db, per training data row and adds the value to total
-      dj_db += err
-    # How numpy works, this divides each parameter column by the training examples total 'm'
-    dj_db = dj_db / m
-    dj_dw = dj_dw / m
-    return dj_db, dj_dw
-
 def compute_gradient_vectorized_logistic(X, y, w, b, sigmoid):
     m = X.shape[0]  # number of examples
     # Compute predictions using sigmoid
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import numpy as np
 
 sys.path.insert(0, os.path.abspath('../../'))
@@ -8,6 +9,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 from matplotlib import cm
 from src.utils.plot_constants import dlc
 from src.models.train_model import compute_cost_logistic_sq_err
+from src.models.predict_model import sigmoid
 
 '''
 Includes common ploting functions.
@@ -179,3 +181,30 @@ def plt_two_logistic_curves_loss():
     plt.suptitle("Loss Curves for Two Categorical Target Values", fontsize=12)
     plt.tight_layout()
     plt.show()
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    """ truncates color map """
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
+def plt_prob(ax, w_out,b_out):
+    """ plots a decision boundary but include shading to indicate the probability """
+    #setup useful ranges and common linspaces
+    x0_space  = np.linspace(0, 4 , 100)
+    x1_space  = np.linspace(0, 4 , 100)
+
+    # get probability for x0,x1 ranges
+    tmp_x0,tmp_x1 = np.meshgrid(x0_space,x1_space)
+    z = np.zeros_like(tmp_x0)
+    for i in range(tmp_x0.shape[0]):
+        for j in range(tmp_x1.shape[1]):
+            z[i,j] = sigmoid(np.dot(w_out, np.array([tmp_x0[i,j],tmp_x1[i,j]])) + b_out)
+
+    cmap = plt.get_cmap('Blues')
+    new_cmap = truncate_colormap(cmap, 0.0, 0.5)
+    pcm = ax.pcolormesh(tmp_x0, tmp_x1, z,
+                   norm=cm.colors.Normalize(vmin=0, vmax=1),
+                   cmap=new_cmap, shading='nearest', alpha = 0.9)
+    ax.figure.colorbar(pcm, ax=ax)
